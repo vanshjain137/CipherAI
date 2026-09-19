5149a7988812345174e215b00ec120b0f77d6af1
Vansh Jain <vanshjain181@gmail.com>
2026-09-19 14:59:05 +0530

    Update db configs for isolated database connections

--- backend/services/auth/config/db.js
+++ backend/services/auth/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.AUTH_MONGO_URI || process.env.MONGODB_URI)
         console.log("db connected")
     } catch (error) {
         console.log(error)
